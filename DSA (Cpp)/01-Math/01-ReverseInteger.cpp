c51bd801a7d7f723a9967b33b7161d29c882b564
Karan Suthar <itskaransuthar28@gmail.com>
2026-05-15 09:13:19 +0530

    Digit Manipulation

---
+++ DSA (Cpp)/01-Math/01-ReverseInteger.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int num = 0;
+        while(x)
+        {
+            if(!(INT_MIN/10 <= num && num <= INT_MAX/10)) return 0;
+            num = num*10 + (x%10);
+            x /= 10;
+        }
+
+        return num;
+    }
+};
--- DSA (Cpp)/01. Introduction to Cpp/01-first.cpp
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    int a = 10;
-    float b = 3.14f;
-    double d = 3.14159;
-    char c = 'K';
-
-    return 0;
-}
