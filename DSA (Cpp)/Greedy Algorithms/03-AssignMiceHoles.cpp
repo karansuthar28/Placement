ad8a9255545de63bbc8fb3166ef3b82e12301449
Karan Suthar <itskaransuthar28@gmail.com>
2026-08-24 16:18:03 +0530

    DSA

--- DSA (Cpp)/Greedy Algorithms/03-AssignMiceHoles.cpp
+++ DSA (Cpp)/Greedy Algorithms/03-AssignMiceHoles.cpp
@@ -5,9 +5,9 @@ class Solution {
   public:
     int assignHole(vector<int>& mices, vector<int>& holes) {
         // code here
+        int answer = 0;
         sort(mices.begin(), mices.end());
         sort(holes.begin(), holes.end());
-        int answer = 0;
         
         for(int i = 0; i < mices.size(); i++) {
             answer = max(answer, abs(mices[i] - holes[i]));
